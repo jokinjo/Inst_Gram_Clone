bc3fd8521f94deee3dca8d075c5b17ac38fa219d
JMS <jokinjo2001@hotmail.com>
2019-02-18 04:27:57 -0500

    End of Section.08 Lesson45 - Try using the onClick method attribute of SAVE (i.e., NOT an onClickListener.)

--- app/src/main/java/com/example/inst_gram_clone/SignUP.java
+++ app/src/main/java/com/example/inst_gram_clone/SignUP.java
@@ -10,9 +10,8 @@ import android.widget.Toast;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.SaveCallback;
-import com.shashank.sony.fancytoastlib.FancyToast;
 
-public class SignUP extends AppCompatActivity implements View.OnClickListener {
+public class SignUP extends AppCompatActivity  {
 
     private TextView btnClicked;
     private EditText editTextN;
@@ -35,56 +34,56 @@ public class SignUP extends AppCompatActivity implements View.OnClickListener {
         editTextPP = findViewById(R.id.editText4);
         editTextPT = findViewById(R.id.editText5);
 
-        btnClicked.setOnClickListener(SignUP.this);
+//        btnClicked.setOnClickListener(SignUP.this);
 
     }
 
-    @Override
-    public void onClick(View view) {
-        try {
+//    @Override
+//    public void onClick(View view) {
+//        try {
+//
+//            final ParseObject kickBoxer = new ParseObject("KickBoxer");
+//            kickBoxer.put("Name", editTextN.getText().toString());
+//            kickBoxer.put("kick_speed", Integer.parseInt(editTextKP.getText().toString()));
+//            kickBoxer.put("kick_strength", Integer.parseInt(editTextKT.getText().toString()));
+//            kickBoxer.put("punch_speed", Integer.parseInt(editTextPP.getText().toString()));
+//            kickBoxer.put("punch_strength", Integer.parseInt(editTextPT.getText().toString()));
+//
+//            kickBoxer.saveInBackground(new SaveCallback() {
+//                @Override
+//                public void done(ParseException e) {
+//                    if (e == null) {
+//                        FancyToast.makeText(SignUP.this, "kickBoxer: " + kickBoxer.get("Name") + " saved to server", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+//                        //                    Toast.makeText(SignUP.this, "kickBoxer object saved sucessfully", Toast.LENGTH_LONG).show();
+//                    } else {
+//                        Toast.makeText(SignUP.this, e.getMessage(), Toast.LENGTH_LONG).show();
+//                    }
+//                }
+//            });
+//        } catch (Exception e) {
+//            FancyToast.makeText(SignUP.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true);
+//        }
+//    }
+//}
 
-            final ParseObject kickBoxer = new ParseObject("KickBoxer");
-            kickBoxer.put("Name", editTextN.getText().toString());
-            kickBoxer.put("kick_speed", Integer.parseInt(editTextKP.getText().toString()));
-            kickBoxer.put("kick_strength", Integer.parseInt(editTextKT.getText().toString()));
-            kickBoxer.put("punch_speed", Integer.parseInt(editTextPP.getText().toString()));
-            kickBoxer.put("punch_strength", Integer.parseInt(editTextPT.getText().toString()));
 
-            kickBoxer.saveInBackground(new SaveCallback() {
-                @Override
-                public void done(ParseException e) {
-                    if (e == null) {
-                        FancyToast.makeText(SignUP.this, "kickBoxer: " + kickBoxer.get("Name") + " saved to server", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
-                        //                    Toast.makeText(SignUP.this, "kickBoxer object saved sucessfully", Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(SignUP.this, e.getMessage(), Toast.LENGTH_LONG).show();
+    public void btnSaveClicked(View view) {
+        ParseObject kickBoxer = new ParseObject("KickBoxer");
+        kickBoxer.put("Name", editTextN.getText().toString());
+        kickBoxer.put("kick_speed", Integer.parseInt(editTextKP.getText().toString()));
+        kickBoxer.put("kick_strength", Integer.parseInt(editTextKT.getText().toString()));
+        kickBoxer.put("punch_speed", Integer.parseInt(editTextPP.getText().toString()));
+        kickBoxer.put("punch_strength", Integer.parseInt(editTextPT.getText().toString()));
+            try {
+                kickBoxer.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null)
+                            Toast.makeText(SignUP.this, "kickBoxer object saved sucessfully", Toast.LENGTH_LONG).show();
                     }
-                }
-            });
-        } catch (Exception e) {
-            FancyToast.makeText(SignUP.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true);
-        }
+                });
+            } catch (Exception e) {
+                Toast.makeText(SignUP.this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
     }
 }
-
-
-//    public void btnSaveClicked(View view) {
-//        ParseObject kickBoxer = new ParseObject("KickBoxer");
-//        kickBoxer.put("Name", editTextN.getText().toString());
-//        kickBoxer.put("kick_speed", editTextKP.getText().toString());
-////        kickBoxer.put("kick_strength", editTextKT.getText());
-////        kickBoxer.put("punch_speed", editTextPP.getText());
-////        kickBoxer.put("punch_strength", editTextPT.getText());
-//            try {
-//                kickBoxer.saveInBackground(new SaveCallback() {
-//                    @Override
-//                    public void done(ParseException e) {
-//                        if (e == null)
-//                            Toast.makeText(SignUP.this, "kickBoxer object saved sucessfully", Toast.LENGTH_LONG).show();
-//                    }
-//                });
-//            } catch (Exception e) {
-//                Toast.makeText(SignUP.this, e.getMessage(), Toast.LENGTH_LONG).show();
-//            }
-//    }
-//}}
