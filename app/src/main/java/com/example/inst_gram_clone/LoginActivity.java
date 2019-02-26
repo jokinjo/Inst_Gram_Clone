d6361a8de72e6a9c1c03191d43fc9ef35dd371a9
JMS <jokinjo2001@hotmail.com>
2019-02-26 11:35:59 -0500

    Crash switching to LogIn from the initial SignUp srceen - Sec.08 Less048 @1min

--- app/src/main/java/com/example/inst_gram_clone/LoginActivity.java
+++ app/src/main/java/com/example/inst_gram_clone/LoginActivity.java
@@ -1,13 +1,65 @@
 package com.example.inst_gram_clone;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText edtEMail, edtPW, edtSignUp, edtLogIn;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle("Sign Up");
+
+        edtEMail = findViewById(R.id.edtEMail_LI);
+        edtPW = findViewById(R.id.edtPW_LI);
+        edtSignUp = findViewById(R.id.edtSignUp_LI);
+        edtLogIn = findViewById(R.id.edtLogIn_LI);
+
+        edtSignUp.setOnClickListener(LoginActivity.this);
+        edtLogIn.setOnClickListener(LoginActivity.this);
+
+        if (ParseUser.getCurrentUser() != null)  if (ParseUser.getCurrentUser() != null) ParseUser.getCurrentUser().logOut();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case (R.id.edtLogIn_LI):
+//                ParseUser parseUser = new ParseUser();
+//                parseUser.
+                ParseUser.logInInBackground(edtEMail.getText().toString(), edtPW.getText().toString(),
+                        new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null && e == null) {
+                            FancyToast.makeText(LoginActivity.this, " user: " + user.getUsername().toString() + " is logged in.", FancyToast.LENGTH_LONG, FancyToast.SUCCESS,false);
+                        } else {
+                            FancyToast.makeText(LoginActivity.this, e.getMessage(), FancyToast.ERROR, FancyToast.LENGTH_LONG, true);
+                        }
+                    }
+                });
+                break;
+            case (R.id.edtSignUp_LI):
+                Intent intent = new Intent(LoginActivity.this, SignUP.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/inst_gram_clone/SignUP.java
+++ app/src/main/java/com/example/inst_gram_clone/SignUP.java
@@ -1,14 +1,20 @@
 package com.example.inst_gram_clone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
 public class SignUP extends AppCompatActivity {
 
-    EditText edtEMail, edtUserName, edtPW, edtSignUp, edtLogIn;
+    private EditText edtEMail, edtUserName, edtPW, edtSignUp, edtLogIn;
 
     private static final String TAG = "SignUP";
 
@@ -17,30 +23,52 @@ public class SignUP extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
         getSupportActionBar().setTitle("Sign Up");
 
-//        setVisible(true);
-//        setTitle("Sign Up");
-     edtEMail = findViewById(R.id.edtEMail);
-     edtUserName = findViewById(R.id.edtUserName);
-     edtPW = findViewById(R.id.edtPW);
-     edtSignUp = findViewById(R.id.edtSignUp);
-     edtLogIn = findViewById(R.id.edtLogIn);
+        edtEMail = findViewById(R.id.edtEMail);
+        edtUserName = findViewById(R.id.edtUserName);
+        edtPW = findViewById(R.id.edtPW);
+        edtSignUp = findViewById(R.id.edtSignUp);
+        edtLogIn = findViewById(R.id.edtLogIn);
 
-     edtSignUp.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
-//             SignUp person
-         }
-     });
-     edtLogIn.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
-//             Log In person
-         }
-      });
+        if (ParseUser.getCurrentUser() != null) ParseUser.getCurrentUser().logOut();
+
+        edtSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ParseUser parseUser = new ParseUser();
+                parseUser.setUsername(edtUserName.getText().toString());
+                parseUser.setEmail(edtEMail.getText().toString());
+                parseUser.setPassword(edtPW.getText().toString());
+
+                parseUser.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+
+                        if (e == null) {
+                            FancyToast.makeText(SignUP.this, parseUser.getUsername() + " is logged in", FancyToast.LENGTH_LONG);
+                        } else {
+                            FancyToast.makeText(SignUP.this, e.getMessage(), FancyToast.LENGTH_LONG);
+                        }
+                    }
+                });
+            }
+        });
+        edtLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(SignUP.this, LoginActivity.class);
+                startActivity(intent);
+
+                //            ParseUser parseUser = new ParseUser();
+                //            parseUser.setUsername(edtUserName.getText().toString());
+                //            parseUser.setEmail(edtEMail.getText().toString());
+                //            parseUser.setPassword(edtPW.getText().toString());
+            }
+        });
     }
 
 
